9e7bd0ccb752bef79a4d96357be367c999431d44
Samrruddhi <112799065+Samrruddhi@users.noreply.github.com>
2024-03-20 17:50:21 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Jenkins Project for Technogeeks, Pune Technologies";
+        return "DevOps Jenkinsci cd Project for Technogeeks, Pune Technologies";
     }
 }
